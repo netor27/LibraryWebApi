80241308e03ec21a2f0a5ef9b4d27e890b292c2d
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-10-23 18:47:07 +0000

    Adding Book Put, Patch and Delete API

--- app.js
+++ app.js
@@ -3,51 +3,15 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
-var app = express();
-
 var db = mongoose.connect('mongodb://libraryUser:LibraryMongoDBPassword@ds229415.mlab.com:29415/library-api-db', { useMongoClient: true });
-var Book = require('./models/bookModel');
 
+var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
-
-bookRouter.route('/Books')
-    .post(function(req, res) {
-        var book = new Book(req.body);
-        book.save();
-        res.status(201).send(book)
-    })
-    .get(function(req, res) {
-        var query = [];
-
-        if (req.query.genre) {
-            query.genre = req.query.genre
-        }
-
-        Book.find(query, function(err, books) {
-            if (err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        });
-    });
-
-bookRouter.route('/Books/:bookId')
-    .get(function(req, res) {
-        Book.findById(req.params.bookId, function(err, book) {
-            if (err)
-                res.status(500).send(err);
-            else
-                res.json(book);
-        });
-    });
-
-
-app.use('/api', bookRouter);
-
-
+var bookRouter = require('./Routes/bookRoutes')();
+app.use('/api/books', bookRouter);
+app.use('/api/authors', bookRouter);
 
 app.get('/', function(req, res) {
     res.send('welcome to my API!');
