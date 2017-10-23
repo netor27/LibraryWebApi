0343e201cb71c7edfa97f7a09cf8636953d1538f
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-10-23 18:19:45 +0000

    Adding Book Post

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+var app = express();
 
 var db = mongoose.connect('mongodb://libraryUser:LibraryMongoDBPassword@ds229415.mlab.com:29415/library-api-db', { useMongoClient: true });
 var Book = require('./models/bookModel');
 
-var app = express();
-
-var port = process.env.PORT || 3000;
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
+    .post(function(req, res) {
+        var book = new Book(req.body);
+        book.save();
+        res.status(201).send(book)
+    })
     .get(function(req, res) {
         var query = [];
-        
-        if(req.query.genre){
+
+        if (req.query.genre) {
             query.genre = req.query.genre
         }
-        
+
         Book.find(query, function(err, books) {
             if (err)
                 res.status(500).send(err);
@@ -26,7 +33,7 @@ bookRouter.route('/Books')
                 res.json(books);
         });
     });
-    
+
 bookRouter.route('/Books/:bookId')
     .get(function(req, res) {
         Book.findById(req.params.bookId, function(err, book) {
@@ -36,7 +43,7 @@ bookRouter.route('/Books/:bookId')
                 res.json(book);
         });
     });
-    
+
 
 app.use('/api', bookRouter);
 
